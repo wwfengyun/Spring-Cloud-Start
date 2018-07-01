fe00a871af70816194448745f825eebd4735d233
wwfengyun <ww_fengyun@126.com>
2018-07-01 16:57:13 +0800

    完善服务发现部分的内容

--- microservice-provider-user/src/main/java/com/windcloud/pro/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/windcloud/pro/controller/UserController.java
@@ -1,19 +1,42 @@
 package com.windcloud.pro.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.windcloud.pro.entity.User;
 import com.windcloud.pro.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/simple/{id}")
     public User findById(@PathVariable Long id) {
         return this.userRepository.findById(id).get();
+    }
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("microservice-provider-user", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/getInstancesInfo")
+    public List<String> getInstancesInfo() {
+        List<String> instancesInfo = this.discoveryClient.getServices();
+        return instancesInfo;
     }
 }
